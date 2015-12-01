7359cd263660bfddb2cccc80af63b634e3d8bd69
bpapn <bpapn@allstate.com>
2015-12-01 11:46:47 +0530

    second commt

--- AppDelegate.m
+++ AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    
+    NSLog(@"inside the appdelegate .m file ");
     
     self.window=[[UIWindow alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
     self.your=[[DatePickerViewController alloc]initWithNibName:@"DatePickerViewController" bundle:nil];
